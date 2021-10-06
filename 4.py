869fd01a795a74562f2535a5f51ce0f5bfeee75c
jungeun919 <renee8369@likelion.org>
2021-10-06 23:31:22 +0900

    10/06

---
+++ 4.py
@@ -0,0 +1,9 @@
+# 다음 변수 a를 print(type(a))로 넣었을 때 출력될 값과의 연결이 알맞지 않은 것은?
+
+# 1)  입력 : a =1,   출력 : class 'int'
+# 2)  입력 : a = 2.22,   출력 : class 'float'
+# 3)  입력 : a = 'p',   출력 : class 'char'
+# 4)  입력 : a = [1, 2, 3],   출력 : class 'list'
+
+print("정답은 3")
+print("왜냐: char->str")
