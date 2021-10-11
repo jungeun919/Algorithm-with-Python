65d677ead672b0019132ec865766ba9f7b46a826
jungeun919 <renee8369@likelion.org>
2021-10-11 23:30:22 +0900

    10번 풀이 추가

--- 10.py
+++ 10.py
@@ -1,17 +1,16 @@
-# 별 찍기
-
-# 입출력 예시
-# >> 입력
-# 5
-
-# >> 출력
-#     *
-#    ***
-#   *****
-#  *******
-# *********
-
+print("[방법1]")
 n = int(input())
 
 for i in range(1, n+1):
 	print(" " * (n-i) + "*" * (2*i-1))
+
+
+print("[방법2]")
+n = int(input())
+
+for i in range(1, n+1):
+	for j in range(n-i):
+		print(" ", end='')
+	for j in range(i):
+		print("* ", end='')
+	print("")
