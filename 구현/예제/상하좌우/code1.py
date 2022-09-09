f5254afc79896dff0e1f0bc6c24037877e2ae9ec
Jungeun Choi <jungchoi@c5r8s7.42seoul.kr>
2022-09-09 14:14:46 +0900

    구현 실전문제

---
+++ 구현/예제/상하좌우/code1.py
@@ -0,0 +1,22 @@
+# n : 공간의 크기
+n = int(input())
+x, y, = 1, 1
+plans = input().split()
+# plans = list(input().split())
+
+# Left Right Up Down
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for plan in plans:
+	for i in range(len(move_types)):
+		if plan == move_types[i]:
+			nx = x + dx[i]
+			ny = y + dy[i]
+			
+	if nx < 1 or ny < 1 or nx > n or ny > n:
+		continue
+	x, y = nx, ny
+	
+print(x, y)
