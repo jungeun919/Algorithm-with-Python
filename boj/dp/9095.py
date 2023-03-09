a21cb325a9dc49104a517e1547a58bf30e9f5acd
jungeun919 <renee8369@likelion.org>
2023-03-09 21:13:34 +0900

    230309

---
+++ boj/dp/9095.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    dp = [0, 1, 2, 4]
+
+    for i in range(4, n + 1):
+        dp.append(dp[i - 3] + dp[i - 2] + dp[i - 1])
+    
+    print(dp[n])
