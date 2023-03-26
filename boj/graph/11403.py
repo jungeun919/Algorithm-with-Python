f9b81d846424a612a669da0ee9f5f0afe174b3db
jungeun919 <renee8369@likelion.org>
2023-03-26 22:58:05 +0900

    230326

---
+++ boj/graph/11403.py
@@ -0,0 +1,17 @@
+# 플로이드 워셜
+n = int(input())
+
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+    
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] == 1 or (graph[i][k] == 1 and graph[k][j] == 1):
+                graph[i][j] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(graph[i][j], end=" ")
+    print()
