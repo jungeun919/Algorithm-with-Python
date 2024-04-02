6f3bb0f6326153f098717221163ad6013a87eda3
jungeun919 <renee8369@likelion.org>
2024-04-02 16:05:14 +0900

    240402

--- Baekjoon/Implementation/14499.py
+++ Baekjoon/Implementation/14499.py
@@ -4,16 +4,40 @@ input = sys.stdin.readline
 
 n, m, x, y, k = map(int, input().split())
 
-dice = []
+board = []
 for _ in range(n):
-	dice.append(list(map(int, input().split())))
+    board.append(list(map(int, input().split())))
 
-# 동(1) 서(2) 북(3) 남(4)
-commands = list(map(int, input().split()))
+dir = list(map(int, input().split()))
 
-#     동  서  북  남
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+#       위 뒤  우 왼  앞 아래
+dice = [0, 0, 0, 0, 0, 0]
 
-for i in range(k):
-	print(commands[i], end=" ")
+def turn(d, dice):
+    if d == 1: # 동
+       return [dice[3], dice[1], dice[0], dice[5], dice[4], dice[2]]
+    elif d == 2: # 서
+        return [dice[2], dice[1], dice[5], dice[0], dice[4], dice[3]]
+    elif d == 3: # 남
+        return [dice[4], dice[0], dice[2], dice[3], dice[5], dice[1]]
+    elif d == 4: # 북
+        return [dice[1], dice[5], dice[2], dice[3], dice[0], dice[4]]
+
+#        동  서  남  북
+dx = [0, 0, 0, -1, 1]
+dy = [0, 1, -1, 0, 0]
+
+for d in dir:
+    nx = x + dx[d]
+    ny = y + dy[d]
+    if not(0 <= nx < n and 0 <= ny < m):
+        continue
+    dice = turn(d, dice)
+    if board[nx][ny] == 0:
+        board[nx][ny] = dice[-1]
+    else:
+        dice[-1] = board[nx][ny]
+        board[nx][ny] = 0
+
+    print(dice[0])
+    x, y = nx, ny
